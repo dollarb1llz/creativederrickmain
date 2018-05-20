1c13a419998fe8cda9fea1c57dd1d643ca1b7238
dollarb1llz <xd0llarb1llzx@gmail.com>
2018-05-20 13:09:52 -0400

    firsst

---
+++ index.php
@@ -0,0 +1,61 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <title>Derrick's Art</title>
+  <meta name="viewport" content="width=device-width, initial-scale=1" />
+  <link href="./css/reset.css" type="text/css" rel="stylesheet" />
+  <link rel="shortcut icon" href="./img/favicon.ico" type="image/x-icon">
+  <link href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css"
+  rel="stylesheet" integrity="sha384-wvfXpqpZZVQGK6TAh5PVlGOfQNHSoD2xbE+QkPxCAFlNEevoEH3Sl0sibVcOQVnN" crossorigin="anonymous">
+  <link href="https://fonts.googleapis.com/css?family=Dekko" rel="stylesheet">
+  <link href="./css/style.css" type="text/css" rel="stylesheet" />
+</head>
+<body>
+  <!--Header-->
+  <h1>Cheap Art, Videos and Websites</h1>
+  <img src="./img/pennyart.jpg" alt="cheap Art" />
+  <!--Examples-->
+  <h2>Examples</h2>
+  <p>I have not made any art for people yet.</p>
+  <ul>
+    <li>Channel Art</li>
+    <li>Videos</li>
+    <li>Logos</li>
+    <li>Emotes</li>
+    <li>Videos</li>
+  </ul>
+  <!--Tools I use-->
+  <h2>Tools I use</h2>
+  <img src="./img/tools.jpg" alt="Photoshop Premiere pro" />
+  <!--Costs-->
+  <h2>Costs</h2>
+  <p>I am not currently charging anything for my services.</p>
+  <!--About me-->
+  <h2>About Me</h2>
+  <img src="./img/me.jpg" alt="Me" />
+  <a href="http://www.instagram.com/derrickhabenero"><p>Instagram</p></a>
+  <!--Process-->
+  <h2>The Process</h2>
+  <ol>
+    <li>Get in contact with me, Derrick.</li>
+    <li>Discuss what you would like.</li>
+    <li>I will show you what I make.</li>
+    <li>If you like it, we will make a deal. If not, no payments.</li>
+  </ol>
+  <!--Contact-->
+  <h2>Contact Information</h2>
+  <a href="http://www.instagram.com/derrickhabenero"><p>Instagram</p></a>
+  <a href="http://www.twitter.com/derrickhabenero"><p>Twitter</p></a>
+  <a href="http://www.twitch.tv/barefootderrick"><p>Twitch</p></a>
+  <a href="https://discord.gg/zBWJ68G"><p>Discord</p></a>
+  <a href="https://www.youtube.com/channel/UCHWBKOzmSGsLU7eVDQy3VVA?view_as=subscriber"><p>Youtube</p></a>
+  <a href="">derrickhabenero@gmail.com</a>
+  <!--Footer-->
+  <h1>Creative Derrick . com</h1>
+
+  <script
+  			  src="https://code.jquery.com/jquery-3.3.1.min.js"
+  			  integrity="sha256-FgpCb/KJQlLNfOu91ta32o/NMZxltwRo8QtmkMRdAu8="
+  			  crossorigin="anonymous"></script>
+</body>
+</html>
